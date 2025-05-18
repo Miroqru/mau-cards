e3106f55427e1c0bca0e58f5b762cc7a139f7871
Milinuri Nirvalen <pentergust@mail.ru>
2025-05-18 17:02:53 +0400

    Remove python scripts

--- tester.py
+++
@@ -1,7 +0,0 @@
-import requests
-from mau.deck.presets import DeckGenerator
-from tqdm import tqdm
-
-card_gen = DeckGenerator.from_preset("classic")
-for card in tqdm(card_gen._cards(), total=108):
-    res = requests.get(f"http://127.0.0.1:3112/card/{card.pack()}/false")
